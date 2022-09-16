e682858598d188c6a1a5258ca7c8781bfe2051a3
IsraelChidera <israelchidera54@gmail.com>
2022-09-16 16:51:45 +0100

    Loaders added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
@@ -5,20 +6,50 @@ import Navbar from './components/Navbar';
 import Checkout from './pages/Checkout';
 import LandingPage from './pages/LandingPage';
 import PricingPage from './pages/PricingPage';
+import './index.css';
 
 function App() {
+  const [loader, setLoader] = useState(false);
+  useEffect(()=>{
+    setTimeout(() => {      
+      setLoader(true);
+    }, 8000)
+  }, [])
+
   return (
     <div className="App bg-white">
-      < Navbar />
+      {
+        !loader?
+        (
+          
+            <div id="load" className="text-8xl  space-x-4">
+              <div>.</div>
+              <div>.</div>
+              <div>A</div>
+              <div>D</div>
+              <div>U</div>
+              <div>Y</div>
+              <div>A</div>
+            </div>
+        
+        )
+        :
+        (
+          <div>            
+            < Navbar />
 
-      <Routes>
-        <Route exact path="/" element={<LandingPage/>}/>
-        <Route path="/plans-pricing" element={<PricingPage/>}/>
-        <Route path="/plans-pricing/payment" element={<Checkout/>}/>
-      </Routes>
+            <Routes>
+              <Route exact path="/" element={<LandingPage/>}/>
+              <Route path="/plans-pricing" element={<PricingPage/>}/>
+              <Route path="/plans-pricing/payment" element={<Checkout/>}/>
+            </Routes>
 
-      < Footer />
+            < Footer />
+          </div>
+        )
+      }
     </div>
+    
   );
 }
 
