14a343efba5b73f9bc219f68aa1edc38caeddf93
IsraelChidera <israelchidera54@gmail.com>
2022-10-18 14:50:26 +0700

    Feature: Removed junk text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -37,28 +37,7 @@ const About = () => {
 
            <Navbar/>
 
-            <div className="mb-10 mt-6 text-black text-center">
-                <h1 className="text-black text-5xl">
-                    About us
-                </h1>
-            </div>
-
-            <section className="mb-4 mx-auto w-4/5 text-center">
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing 
-                    and typesetting industry. Lorem Ipsum has been the 
-                    industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and 
-                    scrambled it to make a type specimen book. It has s
-                    urvived not only five centuries, but also the leap 
-                    into electronic typesetting, remaining essentially 
-                    unchanged. It was popularised in the 1960s with the 
-                    release of Letraset sheets containing Lorem Ipsum p
-                    assages, and more recently with desktop publishing 
-                    software like Aldus PageMaker including versions of 
-                    Lorem Ipsum   
-                </p>
-            </section>
+            
 
             <section>
                 <div className="bg-white py-12">
